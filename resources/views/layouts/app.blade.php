cce25ee764726834a36fae7c7ce4fd9884bdf86b
Alex Mwangi <alex@stl-horizon.com>
2022-03-18 11:53:08 +0300

    Unlinked css/app.css - Not going to use

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,9 +15,7 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
+  
     {{-- Bootstrap5 min css --}}
       <link href="{{ asset('frontend/css/bootstrap5.css') }}" rel="stylesheet">
 
