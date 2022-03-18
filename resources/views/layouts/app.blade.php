4c3bedb219b8e93faf7cf07cc1e38788fc1b61bf
Alex Mwangi <alex@stl-horizon.com>
2022-03-18 11:47:34 +0300

    Linked bootstrap css/js scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,7 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+   
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -18,6 +17,9 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    {{-- Bootstrap5 min css --}}
+      <link href="{{ asset('frontend/css/bootstrap5.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -79,5 +81,9 @@
             @yield('content')
         </main>
     </div>
+
+ <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>
